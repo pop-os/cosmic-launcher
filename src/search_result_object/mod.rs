4b632df7e0e0a33f4464e96180d23d32bea52ea9
Ashley Wulber <Ashley@system76.com>
2022-01-24 11:30:20 -0500

    first commit

---
+++ src/search_result_object/mod.rs
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-only
+use crate::utils::BoxedSearchResult;
+use gtk4::glib;
+use gtk4::prelude::*;
+
+mod imp;
+
+glib::wrapper! {
+    pub struct SearchResultObject(ObjectSubclass<imp::SearchResultObject>);
+}
+
+impl SearchResultObject {
+    pub fn new(search_result: &BoxedSearchResult) -> Self {
+        glib::Object::new(&[("data", search_result)]).expect("Failed to create Application Object")
+    }
+
+    pub fn data(&self) -> Option<pop_launcher::SearchResult> {
+        let search_result = self.property::<BoxedSearchResult>("data");
+        return search_result.0;
+    }
+}
