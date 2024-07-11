07ceaec7cae944ea38a9e692d56121a4a122838c
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-07-12 00:50:31 +0200

    journald support

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,7 @@ use localize::localize;
 use crate::config::VERSION;
 
 fn main() -> cosmic::iced::Result {
-    // Initialize logger
-    #[cfg(feature = "console")]
-    if std::env::var("TOKIO_CONSOLE").as_deref() == Ok("1") {
-        std::env::set_var("RUST_LOG", "trace");
-        console_subscriber::init();
-    }
-    pretty_env_logger::init();
+    init_logging();
 
     info!(
         "cosmic-launcher ({})",
@@ -29,4 +23,33 @@ fn main() -> cosmic::iced::Result {
     localize();
 
     app::run()
+}
+
+fn init_logging() {
+    use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+    // Initialize logger
+    #[cfg(feature = "console")]
+    if std::env::var("TOKIO_CONSOLE").as_deref() == Ok("1") {
+        std::env::set_var("RUST_LOG", "trace");
+        console_subscriber::init();
+    }
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("warn"))
+        .unwrap();
+
+    let fmt_layer = fmt::layer().with_target(false);
+
+    if let Ok(journal_layer) = tracing_journald::layer() {
+        tracing_subscriber::registry()
+            .with(journal_layer)
+            .with(filter_layer)
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(fmt_layer)
+            .with(filter_layer)
+            .init();
+    }
 }
